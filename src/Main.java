e655ed1d786cae21d2476a755451200dea31479c
brunocapim <loginobsequio.com>
2022-05-28 23:41:15 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.desafio.dominio.Curso;
-import br.com.desafio.dominio.Mentoria;
+import br.com.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -17,13 +16,36 @@ public class Main {
         curso2.setDescricao("Descricao course JavaScript");
         curso2.setCargaHoraria(4);
 
+
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Dev");
+        bootcamp.setDescricao("Descricao Bootcamp Java Dev");
+        bootcamp.getConteudo().add(curso1);
+        bootcamp.getConteudo().add(curso2);
+
+        Dev devBruno = new Dev();
+        devBruno.setNome("Bruno");
+        devBruno.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Bruno: "+devBruno.getConteudoInscritos());
+        devBruno.progredir();
+        System.out.println("Conteudos Concluidos Bruno: "+devBruno.getConteudoConcluido());
+
+        Dev devFelipe = new Dev();
+        devFelipe.setNome("Felpie");
+        devFelipe.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Felipe: "+devFelipe.getConteudoInscritos());
+        devFelipe.progredir();
+        System.out.println("Conteudos Concluidos Felipe: "+devFelipe.getConteudoConcluido());
+
     }
 }
